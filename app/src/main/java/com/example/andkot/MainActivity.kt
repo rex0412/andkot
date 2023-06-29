0930f92fe8ae48c0a618898421b0f10b2aa73bfa
rex0412 <rex041200@naver.com>
2023-06-29 11:31:38 +0900

    text change

--- app/src/main/java/com/example/andkot/MainActivity.kt
+++ app/src/main/java/com/example/andkot/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.andkot
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Android Start"
     }
 }
