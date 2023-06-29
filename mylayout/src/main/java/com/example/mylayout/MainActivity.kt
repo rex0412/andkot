ad020fe7cb8f1f0cec7d4d0cb56af4a9c7d3621c
rex0412 <rex041200@naver.com>
2023-06-29 11:42:10 +0900

    module added

---
+++ mylayout/src/main/java/com/example/mylayout/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mylayout
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Android Module Start"
+    }
+}
