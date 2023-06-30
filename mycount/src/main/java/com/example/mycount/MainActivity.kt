850c030fdcae30cd5a324ec8366b9ae792cebc38
rex0412 <rex041200@naver.com>
2023-06-30 15:36:10 +0900

    Number Count

--- mycount/src/main/java/com/example/mycount/MainActivity.kt
+++ mycount/src/main/java/com/example/mycount/MainActivity.kt
@@ -2,45 +2,28 @@ package com.example.mycount
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mycount.ui.theme.AndKotTheme
+import com.example.mycount.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndKotTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.countButton.setOnClickListener {
+            count++
+            binding.countText.text = "$count"
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        binding.countButton2.setOnClickListener {
+            count--
+            binding.countText.text = "$count"
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndKotTheme {
-        Greeting("Android")
+        binding.CountButton3.setOnClickListener {
+            count = 0
+            binding.countText.text = "$count"
+        }
     }
 }
