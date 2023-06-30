84ffd2100b2336ff5a9a94aaf557ffd2fbe59875
rex0412 <rex041200@naver.com>
2023-06-30 15:52:19 +0900

    Time Out Number Count

--- mycount/src/main/java/com/example/mycount/MainActivity.kt
+++ mycount/src/main/java/com/example/mycount/MainActivity.kt
@@ -6,14 +6,22 @@ import com.example.mycount.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
     var count = 0
+    var initTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.countButton.setOnClickListener {
-            count++
-            binding.countText.text = "$count"
+            if (count == 0) {
+                initTime = System.currentTimeMillis()
+            }
+            var timeDiff = System.currentTimeMillis() - initTime
+            var isAvailable : Boolean =  timeDiff < 10 * 1000
+            if (isAvailable) {
+                count++
+                binding.countText.text = "$count"
+            }
         }
 
         binding.countButton2.setOnClickListener {
